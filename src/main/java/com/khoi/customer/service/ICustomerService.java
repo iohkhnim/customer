fa28d4e92681d4c31254ca5704d30e17dd75bef6
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-14 16:40:23 +0700

    50% create order

--- src/main/java/com/khoi/customer/service/ICustomerService.java
+++ src/main/java/com/khoi/customer/service/ICustomerService.java
@@ -1,8 +1,9 @@
 package com.khoi.customer.service;
 
 import com.khoi.basecrud.service.IBaseService;
+import com.khoi.customer.dto.Checkout;
 import com.khoi.customer.dto.Customer;
 
 public interface ICustomerService extends IBaseService<Customer, Integer> {
-
+  Boolean createOrder(Checkout checkout);
 }
--- src/main/java/com/khoi/customer/service/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/khoi/customer/service/service/impl/CustomerServiceImpl.java
@@ -1,12 +1,53 @@
 package com.khoi.customer.service.service.impl;
 
 import com.khoi.basecrud.service.service.impl.BaseServiceImpl;
+import com.khoi.customer.dto.Checkout;
+import com.khoi.customer.dto.CheckoutData;
 import com.khoi.customer.dto.Customer;
 import com.khoi.customer.service.ICustomerService;
+import com.khoi.orderproto.CreateOrderRequest;
+import com.khoi.orderproto.CreateOrderResponse;
+import com.khoi.orderproto.OrderServiceGrpc;
+import com.khoi.proto.GetPriceRequest;
+import com.khoi.proto.GetPriceResponse;
+import com.khoi.proto.PriceServiceGrpc;
+import com.khoi.stockproto.StockServiceGrpc;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer> implements
     ICustomerService {
 
+  @Qualifier("priceService")
+  private final PriceServiceGrpc.PriceServiceBlockingStub priceService;
+
+  @Qualifier("stockService")
+  private final StockServiceGrpc.StockServiceBlockingStub stockService;
+
+  @Qualifier("orderService")
+  private final OrderServiceGrpc.OrderServiceBlockingStub orderService;
+
+  public CustomerServiceImpl(OrderServiceGrpc.OrderServiceBlockingStub orderService,
+      PriceServiceGrpc.PriceServiceBlockingStub priceService,
+      StockServiceGrpc.StockServiceBlockingStub stockService) {
+    this.orderService = orderService;
+    this.priceService = priceService;
+    this.stockService = stockService;
+  }
+
+  @Override
+  public Boolean createOrder(Checkout checkout) {
+    CreateOrderResponse rs = orderService.createOrder(
+        CreateOrderRequest.newBuilder().setCustomerId(checkout.getCustomer_id()).build());
+    if (rs.getOrderId() > 0) {
+      for (CheckoutData data : checkout.getProducts()) {
+        GetPriceResponse priceResponse = priceService
+            .getPrice(GetPriceRequest.newBuilder().setProductId(data.getProduct_id()).build());
+      }
+    } else {
+      return false;
+    }
+    return false;
+  }
 }
