b27f2a6b4a15bb68d60c5634e7ddf6fbef2ddeec
iohnkhnim <hiepsi1211@gmail.com>
2019-03-19 00:07:14 +0700

    put all checkout logical into order instead of customer

--- src/main/java/com/khoi/customer/service/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/khoi/customer/service/service/impl/CustomerServiceImpl.java
@@ -5,27 +5,20 @@ import com.khoi.customer.dto.Checkout;
 import com.khoi.customer.dto.CheckoutData;
 import com.khoi.customer.dto.Customer;
 import com.khoi.customer.service.ICustomerService;
-import com.khoi.orderproto.CreateOrderItemRequest;
-import com.khoi.orderproto.CreateOrderItemResponse;
-import com.khoi.orderproto.CreateOrderRequest;
-import com.khoi.orderproto.CreateOrderResponse;
-import com.khoi.orderproto.OrderServiceGrpc;
+import com.khoi.orderproto.*;
 import com.khoi.proto.GetPriceRequest;
 import com.khoi.proto.GetPriceResponse;
 import com.khoi.proto.PriceServiceGrpc;
-import com.khoi.stockproto.GetBestStockRequest;
-import com.khoi.stockproto.GetBestStockResponse;
-import com.khoi.stockproto.StockServiceGrpc;
-import com.khoi.stockproto.SubtractRequest;
-import com.khoi.stockproto.SubtractResponse;
-import java.util.List;
-import java.util.function.Consumer;
+import com.khoi.stockproto.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer> implements
-    ICustomerService {
+public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer>
+    implements ICustomerService {
 
   @Qualifier("priceService")
   private final PriceServiceGrpc.PriceServiceBlockingStub priceService;
@@ -36,7 +29,8 @@ public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer> impl
   @Qualifier("orderService")
   private final OrderServiceGrpc.OrderServiceBlockingStub orderService;
 
-  public CustomerServiceImpl(OrderServiceGrpc.OrderServiceBlockingStub orderService,
+  public CustomerServiceImpl(
+      OrderServiceGrpc.OrderServiceBlockingStub orderService,
       PriceServiceGrpc.PriceServiceBlockingStub priceService,
       StockServiceGrpc.StockServiceBlockingStub stockService) {
     this.orderService = orderService;
@@ -46,34 +40,59 @@ public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer> impl
 
   @Override
   public Boolean createOrder(Checkout checkout) {
-
-    //create an order
-    CreateOrderResponse rs = orderService.createOrder(
-        CreateOrderRequest.newBuilder().setCustomerId(checkout.getCustomer_id()).build());
     List<CheckoutData> list = checkout.getProducts();
+    List<CheckoutDataProto> checkoutDataProtos = new ArrayList<>();
 
-    if (rs.getOrderId() > 0) { //check if order is successfully created
+    list.stream()
+        .forEach(
+            p ->
+                checkoutDataProtos.add(
+                    CheckoutDataProto.newBuilder()
+                        .setProductId(p.getProduct_id())
+                        .setAmount(p.getAmount())
+                        .build()));
+    // create an order
+    CreateOrderResponse rs =
+        orderService.createOrder(
+            CreateOrderRequest.newBuilder()
+                .setCustomerId(checkout.getCustomer_id())
+                .addAllCheckoutDataProto(checkoutDataProtos)
+                .build());
+
+    /*if (rs.getOrderId() > 0) { // check if order is successfully created
       for (CheckoutData data : list) {
-        GetBestStockResponse bestStock = stockService.getBestStock(
-            GetBestStockRequest.newBuilder().setProductId(data.getProduct_id())
-                .setAmount(data.getAmount()).build());
-        if (bestStock.getStockId() <= 0) { //if there's not enough stock
+        GetBestStockResponse bestStock =
+            stockService.getBestStock(
+                GetBestStockRequest.newBuilder()
+                    .setProductId(data.getProduct_id())
+                    .setAmount(data.getAmount())
+                    .build());
+        if (bestStock.getStockId() <= 0) { // if there's not enough stock
           continue;
         }
-        GetPriceResponse priceResponse = priceService
-            .getPrice(GetPriceRequest.newBuilder().setProductId(data.getProduct_id()).build());
+        GetPriceResponse priceResponse =
+            priceService.getPrice(
+                GetPriceRequest.newBuilder().setProductId(data.getProduct_id()).build());
 
-        //create order_item
-        CreateOrderItemResponse orderItemResponse = orderService
-            .createOrderItem(CreateOrderItemRequest.newBuilder().setOrderId(rs.getOrderId())
-                .setProductId(data.getProduct_id()).setAmount(data.getAmount())
-                .setPrice(priceResponse.getPrice()).setStockId(bestStock.getStockId()).build());
+        // create order_item
+        CreateOrderItemResponse orderItemResponse =
+            orderService.createOrderItem(
+                CreateOrderItemRequest.newBuilder()
+                    .setOrderId(rs.getOrderId())
+                    .setProductId(data.getProduct_id())
+                    .setAmount(data.getAmount())
+                    .setPrice(priceResponse.getPrice())
+                    .setStockId(bestStock.getStockId())
+                    .build());
 
-        //subtract stock
-        SubtractResponse subtractResponse = stockService.subtract(
-            SubtractRequest.newBuilder().setStockId(bestStock.getStockId())
-                .setAmount(data.getAmount()).build());
-        if (subtractResponse.getStockId() > 1) { //subtract is completed
+        // subtract stock
+        SubtractResponse subtractResponse =
+            stockService.subtract(
+                SubtractRequest.newBuilder()
+                    .setStockId(bestStock.getStockId())
+                    .setAmount(data.getAmount())
+                    .build());
+        if (subtractResponse.getStockId() > 1) { // subtract is completed
           continue;
         } else {
           return false;
@@ -82,6 +101,7 @@ public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer> impl
       return true;
     } else {
       return false;
-    }
+    }*/
+    return rs.getOrderId() > 0;
   }
 }
